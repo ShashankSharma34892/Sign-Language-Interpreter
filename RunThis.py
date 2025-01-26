4ea0fc48e231dc612d1fdeca3296da2fdc101b45
Shonk <shankkamail@gmail.com>
2025-01-26 22:33:26 +0530

    added to github

---
+++ RunThis.py
@@ -0,0 +1,258 @@
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import mediapipe as mp
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
+
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+
+def draw_styled_landmarks(image, results):
+    mp_drawing.draw_landmarks(
+        image,
+        results.face_landmarks,
+        mp_holistic.FACEMESH_TESSELATION,
+        mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+        mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1),
+    )
+    mp_drawing.draw_landmarks(
+        image,
+        results.pose_landmarks,
+        mp_holistic.POSE_CONNECTIONS,
+        mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+        mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2),
+    )
+    mp_drawing.draw_landmarks(
+        image,
+        results.left_hand_landmarks,
+        mp_holistic.HAND_CONNECTIONS,
+        mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+        mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2),
+    )
+    mp_drawing.draw_landmarks(
+        image,
+        results.right_hand_landmarks,
+        mp_holistic.HAND_CONNECTIONS,
+        mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+        mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2),
+    )
+
+
+def extract_keypoints(results):
+    pose = (
+        np.array(
+            [
+                [res.x, res.y, res.z, res.visibility]
+                for res in results.pose_landmarks.landmark
+            ]
+        ).flatten()
+        if results.pose_landmarks
+        else np.zeros(33 * 4)
+    )
+    lh = (
+        np.array(
+            [[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]
+        ).flatten()
+        if results.left_hand_landmarks
+        else np.zeros(21 * 3)
+    )
+    rh = (
+        np.array(
+            [[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]
+        ).flatten()
+        if results.right_hand_landmarks
+        else np.zeros(21 * 3)
+    )
+    face = (
+        np.array(
+            [[res.x, res.y, res.z] for res in results.face_landmarks.landmark]
+        ).flatten()
+        if results.face_landmarks
+        else np.zeros(468 * 3)
+    )
+    return np.concatenate([pose, face, lh, rh])
+
+
+colors = [(245, 117, 16), (117, 245, 16), (16, 117, 245)]
+
+
+def prob_visualisation(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(
+            output_frame,
+            (3, 60 + num * 40),
+            (int(prob * 100), 90 + num * 40),
+            colors[num],
+            -1,
+        )
+        cv2.putText(
+            output_frame,
+            actions[num],
+            (3, 85 + num * 40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 255),
+            1,
+            cv2.LINE_AA,
+        )
+    return output_frame
+
+
+DATA_PATH = os.path.join("MP_Data")
+# Actions
+actions = np.array(["hello", "thanks", "iloveyou"])
+# 30 videos worth of data
+no_sequences = 30
+# 30 frames
+sequence_length = 30
+
+for action in actions:
+    for sequence in range(no_sequences):
+        try:
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+        except:
+            pass
+
+label_map = {label: num for num, label in enumerate(actions)}
+sequences, labels = [], []
+for action in actions:
+    for sequence in range(no_sequences):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(
+                os.path.join(
+                    DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)
+                )
+            )
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+log_dir = os.path.join("Logs")
+tb_callback = TensorBoard(log_dir=log_dir)
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation="relu", input_shape=(30, 1662)))
+model.add(LSTM(128, return_sequences=True, activation="relu"))
+model.add(LSTM(64, return_sequences=False, activation="relu"))
+model.add(Dense(64, activation="relu"))
+model.add(Dense(32, activation="relu"))
+model.add(Dense(actions.shape[0], activation="softmax"))
+
+res = [0.2, 0.7, 0.01]
+
+actions[np.argmax(res)]
+model.compile(
+    optimizer="Adam", loss="categorical_crossentropy", metrics=["categorical_accuracy"]
+)
+actions[np.argmax(res[1])]
+
+model.load_weights("HISaction.h5")
+
+# New Detection Variables
+sequence = []
+sentence = []
+threshold = 0.7
+
+cap = cv2.VideoCapture(0)
+# Mediapipe Model
+with mp_holistic.Holistic(
+    min_detection_confidence=0.5, min_tracking_confidence=0.5
+) as holistic:
+    while cap.isOpened():
+
+        # Read Feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+
+        # Draw Styled Landmarks
+        draw_styled_landmarks(image, results)
+
+        # Prediciton Logic
+        keypoints = extract_keypoints(results)
+        sequence.insert(0, keypoints)
+        sequence = sequence[:30]
+
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+
+        # Visualization
+        if res[np.argmax(res)] > threshold:
+            if len(sentence) > 0:
+                if actions[np.argmax(res)] != sentence[-1]:
+                    sentence.append(actions[np.argmax(res)])
+            else:
+                sentence.append(actions[np.argmax(res)])
+
+        if len(sentence) > 5:
+            sentence = sentence[-5:]
+
+        # visualisating probability
+        # image = prob_visualisation(res, actions, image, colors)
+
+        # cv2.rectangle(image, (0, 0), (900, 40), (245, 117, 16), -1)
+        # cv2.putText(
+        # image,
+        # " ".join(sentence),
+        # (3, 30),
+        # cv2.FONT_HERSHEY_SIMPLEX,
+        # 1,
+        # (255, 255, 255),
+        # 2,
+        # cv2.LINE_AA,
+        # )
+
+        # Show to Screen
+        # cv2.namedWindow("OpenCV Feed")
+        # cv2.resizeWindow("OpenCV Feed", 620, 500)
+        # cv2.namedWindow("Output")
+        # cv2.resizeWindow("Output", 580, 500)
+        image = cv2.resize(image, (0, 0), fx=1, fy=1)
+
+        # predicimage = cv2.imread("C:/Users/shank/Pictures/c.PNG", 0)
+        output = np.zeros((480, 637, 3), dtype="uint8")
+        output = prob_visualisation(res, actions, output, colors)
+        # cv2.rectangle(
+        # output,
+        # (0, 200),
+        # (500, 100),
+        # colors[0],
+        # -1,
+        # )
+        cv2.putText(
+            output,
+            f"Predicted Text - {actions[np.argmax(res)]}",
+            (3, 20),
+            cv2.FONT_HERSHEY_COMPLEX_SMALL,
+            1,
+            (255, 255, 255),
+            2,
+            cv2.LINE_AA,
+        )
+
+        cv2.imshow("OpenCV Feed", image)
+        cv2.imshow("Output", output)
+
+        # Breaking the Feed
+        if cv2.waitKey(10) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
